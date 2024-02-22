2a88aa1a48757a22fc5b18b2d70224ff6816ced8
Andrew Hutcheson <andrewhutch10@gmail.com>
2024-02-22 07:58:07 -0600

    Change navbar to dynamic grid layout and made reusable grid css class

--- capstone/src/Nav.jsx
+++ capstone/src/Nav.jsx
@@ -2,9 +2,9 @@ import logo from "./images/small_logo.jpg";
 
 function Nav() {
     return (
-        <nav>
-            <img src={logo} width="200" height="50" alt="logo" ></img>
-            <ul>
+        <nav >
+            <ul className="grid-adjustable-columns">
+                <li> <img src={logo} width="200" height="50" alt="logo" ></img></li>
                 <li> <a href="/home"> Home</a> </li>
                 <li> <a href="/about"> About</a> </li>
                 <li> <a href="/menu"> Menu</a> </li>
